df2c5a155ec25b0f2b81f59ea6a9743036dbc2c6
KenethSandoval <kenethra.74@gmail.com>
2022-01-11 22:04:40 -0600

    fix: disabled url web in not urlDemo or urlGithub

--- src/app/models/projects.model.ts
+++ src/app/models/projects.model.ts
@@ -2,17 +2,22 @@ import { environment } from '../../environments/environment';
 const base_url = environment.base_url;
 
 export class Project {
-  constructor(
-    public _id: string,
-    public title: string,
-    public description: string,
-    public urlGithub: string,
-    public tags: Array<string>,
-    public urlDemo: string,
-    public image: string
-  ) {}
+    constructor(
+        public _id: string,
+        public title: string,
+        public description: string,
+        public urlGithub: string,
+        public tags: Array<string>,
+        public urlDemo: string,
+        public image: string
+    ) { }
 
-  get _imageUrl() {
-    return `${base_url}/project/return/${this.image}`;
-  }
+    get _imageUrl() {
+
+        if (!this.image) {
+            return `${base_url}/project/return/no-img.jpg`;
+        } else {
+            return `${base_url}/project/return/${this.image}`;
+        }
+    }
 }

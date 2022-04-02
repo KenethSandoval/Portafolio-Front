784ea08e0be02abcc4dbfa5c4bd2808fc27babda
KenethSandoval <kenethra.74@gmail.com>
2022-04-01 20:58:34 -0600

    feat: modal service done

--- src/app/services/modal.service.ts
+++ src/app/services/modal.service.ts
@@ -1,9 +1,32 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+
+const base_url = environment.base_url;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ModalService {
+	private _hiddenModal: boolean = true;
+	public id!:string;
+	public img!:string;
+
+	public newImg: EventEmitter<string> = new EventEmitter<string>();
+
+	get hiddenModal() {
+		return this._hiddenModal;
+	}
+
+	openModal(id:string, img:string = 'no-image') {
+		this._hiddenModal = false;
+		this.id = id;
+
+		this.img = `${base_url}/uploads/${img}` ;
+	}
+
+	closeModal() {
+		this._hiddenModal = true;
+	}
 
   constructor() { }
 }
